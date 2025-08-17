82b115bded2e9f0042907468f9a89cc334f80b00
RizkyPratama05 <rizkypp428@gmail.com>
2025-08-17 15:50:34 +0700

    percobaan koneksi

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-//angangang
+const db = require("./database/db.js");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`);
+});
