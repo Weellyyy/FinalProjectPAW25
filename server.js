7a180f7c16591726454f2ac558ab7fb13ed65c11
SASCHADDD <saschadanu23@gmail.com>
2025-08-18 00:56:08 +0700

    Menghubungkan server ke index (berandauser.ejs)

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ require('dotenv').config({ path: __dirname + '/.env' });
 const app = express();
 const port = process.env.PORT || 3000;
 
+// 1. Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/view');
+
+// 2. Route for landing page
+app.get('/', (req, res) => {
+    res.render('user/BerandaUser');
+});
+
 app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
 });
