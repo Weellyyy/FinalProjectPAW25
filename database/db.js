258948c6239f22f3eaeb09221dbaabd53a020a97
SASCHADDD <saschadanu23@gmail.com>
2025-08-17 15:56:44 +0700

    port db

--- database/db.js
+++ database/db.js
@@ -2,14 +2,17 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || "127.0.0.1",
+  port: process.env.DB_PORT || 3307, // 
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 
 connection.connect((err) => {
   if (err) {
