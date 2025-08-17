c1ecab9e787ec1430abf7ef7f9490e7d6fed270a
RizkyPratama05 <rizkypp428@gmail.com>
2025-08-17 15:24:23 +0700

    db

---
+++ database/db.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to the MySQL database.");
+});
+
+module.exports = connection;
